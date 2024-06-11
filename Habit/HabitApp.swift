ae1a563b8f7fee962f1b6c6c0605557c2bc756d7
Kaic Cesar <kaic_cesar@hotmail.com>
2024-06-11 15:19:21 -0300

    inicio projeto

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Kaic Cesar on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
